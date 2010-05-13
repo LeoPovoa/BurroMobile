9b2be66619408abcb73b02f8c7617ef6f7faa1fa
Carlos Leonardo Ramos Póvoa <leonardo_povoa@MacBook-de-Carlos-Leonardo-Ramos-Povoa.local>
2010-05-13 15:46:32 -0300

    Commit1

--- Classes/BurroMobileAppDelegate.h
+++ Classes/BurroMobileAppDelegate.h
@@ -3,7 +3,21 @@
 //  BurroMobile
 //
 //  Created by Carlos Leonardo Ramos Póvoa on 01/02/10.
-//  Copyright LogGeo 2010. All rights reserved.
+//  Copyright HAPAX 2010. All rights reserved.
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. (GPL_LICENSE.txt) If not, see <http://www.gnu.org/licenses/>
 //
 
 #import <UIKit/UIKit.h>
